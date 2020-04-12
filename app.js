a3e666ab3f27e15bd74eae1175f4901cb265ac9f
fkn1017 <kosuke.fukano@gmail.com>
2020-04-12 16:57:25 +0900

    POSTのAPIを追加

--- app.js
+++ app.js
@@ -2,11 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json'); 
+    next();
+});
+
+
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
     res.json({
         message: "This is sample api."
     })
+});
+
+app.post('/', (req, res) => {
+
+    try {
+        const body = req.body;
+        res.json({
+            message: "This is sample api.",
+            body: body
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(415);
+        res.json({
+            message: "Bad request",
+            error: error.message
+        });
+    }    
 });
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
